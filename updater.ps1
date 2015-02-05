463b7e770fe992bebf5535fcf1802c141fc7c455
Bennett Perkins <bennettperkins@gmail.com>
2015-02-05 16:28:31 +0800

    Check old_ip != new_ip before updating

--- updater.ps1
+++ updater.ps1
@@ -1,16 +1,39 @@
 $currentPath=Split-Path ((Get-Variable MyInvocation -Scope 0).Value).MyCommand.Path
 Import-Module ("{0}\updater-functions.ps1" -f $currentPath)
 
-$hostname = "HOSTNAME"
+$hostname = "HOSTNAME (fqdn)"
 $password = "PASSWORD"
 $url = "https://dyn.dns.he.net/nic/update?hostname={0}&password={1}" -f $hostname, $password
 
-# To use a custom IP:
-#$myip = @(Get-IPAddresses | where { $_ -match "10.25.6*" })[0]
-#$url = "https://dyn.dns.he.net/nic/update?hostname={0}&password={1}&myip={2}" -f $hostname, $password, $myip
+$regkey = 'HKCU:\Software\bennettp123\dns.he.updater'
+New-Item -Path $regkey -Type directory -Force 2>$null
+
+# get oldip from registry
+$oldip = $( (Get-ItemProperty -path $regkey).oldip 2>$null )
+if (-not $oldip) { $oldip = "UNKNOWN"; }
 
 Ignore-SLL-Errors
 UseUnsafeHeaderParsing
-
 $wc = Get-Webclient
-$wc.DownloadString($url)
+
+# get newip from checkip.dns.he.net
+$regex = 'Your (Proxy)? IPaddress is : ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\(via ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\)?'
+$myip = $( $wc.DownloadString("http://checkip.dns.he.net") | ForEach-Object {
+  if ($_ -match $regex) {
+    if ($matches[4]) { $matches[4]; } else { $matches[3]; }
+  }
+})
+
+# or, to use a custom IP:
+#$myip = @(Get-IPAddresses | where { $_ -match "10.25.64.*" })[0]
+#$url = "https://dyn.dns.he.net/nic/update?hostname={0}&password={1}&myip={2}" -f $hostname, $password, $myip
+
+# quit if oldip=newip
+if ($myip -eq $oldip) { exit 0; }
+
+# send newip to dyn.dns.he.net; save to registry if successful
+$wc.DownloadString($url) | ForEach-Object {
+  if ($_ -match 'good (.*)') { $matches[1]; }
+} | ForEach-Object { 
+  Set-ItemProperty -path $regkey -name oldip -value $_;
+}
